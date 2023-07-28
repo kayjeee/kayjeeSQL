c4947257aabac81526d9422451b2e2d7063c4af7
kayjee <kagiso.killagram@gmail.com>
2023-07-28 20:48:24 +0200

    update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 -- data.sql
---INSERT animals data
+
 -- Create the species table
 CREATE TABLE species (
     id SERIAL PRIMARY KEY,
@@ -49,18 +49,18 @@ CREATE TABLE visits (
 );
 
 -- INSERT animals data
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23),
-       ('Gabumon', '2018-11-15', 2, TRUE, 8),
-       ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-       ('Devimon', '2017-05-12', 5, TRUE, 11),
-       ('Charmander', '2020-02-08', 0, FALSE, -11),
-       ('Plantmon', '2021-11-15', 2, TRUE, -5.7),
-       ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
-       ('Angemon', '2005-01-12', 1, TRUE, -45),
-       ('Boarmon', '2005-01-07', 7, TRUE, 20.4),
-       ('Blossom', '1998-10-13', 3, TRUE, 17),
-       ('Ditto', '2022-05-14', 4, TRUE, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id)
+VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23, 2, 1),
+       ('Gabumon', '2018-11-15', 2, TRUE, 8, 2, 2),
+       ('Pikachu', '2021-01-07', 1, FALSE, 15.04, 1, 2),
+       ('Devimon', '2017-05-12', 5, TRUE, 11, 2, 3),
+       ('Charmander', '2020-02-08', 0, FALSE, -11, 1, 4),
+       ('Plantmon', '2021-11-15', 2, TRUE, -5.7, 2, 3),
+       ('Squirtle', '1993-04-02', 3, FALSE, -12.13, 1, 4),
+       ('Angemon', '2005-01-12', 1, TRUE, -45, 2, 5),
+       ('Boarmon', '2005-01-07', 7, TRUE, 20.4, 2, 5),
+       ('Blossom', '1998-10-13', 3, TRUE, 17, 1, 4),
+       ('Ditto', '2022-05-14', 4, TRUE, 22, 1, NULL);
 
 -- INSERT owners data
 INSERT INTO owners (full_name, age) VALUES 
@@ -73,17 +73,6 @@ INSERT INTO owners (full_name, age) VALUES
 
 -- INSERT species data
 INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
-
--- UPDATE species_id col from animals table using species table
-UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
-
--- UPDATE owner_id col from animals table using owners table
-UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 2 WHERE name IN ('Pikachu', 'Gabumon');
-UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
-UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 
 -- INSERT into vets table
 INSERT INTO vets (name, age, date_of_graduation) VALUES 
@@ -126,21 +115,4 @@ VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM an
 INSERT INTO visits (vet_id, animal_id, visit_date)
 VALUES ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Plantmon'), '2021-04-07');
 INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM animals WHERE name = 'Squirtle'), '2019-09-29');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM animals WHERE name = 'Angemon'), '2020-10-03');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM animals WHERE name = 'Angemon'), '2020-11-04');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2019-01-24');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2019-05-15');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2020-02-27');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2020-08-03');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM animals WHERE name = 'Blossom'), '2020-05-24');
-INSERT INTO visits (vet_id, animal_id, visit_date)
-VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2021-01-11');
-
+VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM animals WHERE name
